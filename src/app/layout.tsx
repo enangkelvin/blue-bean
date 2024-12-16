712b4890a0b23c2501571e74129b235dcc5555f5
enangkelvin <71407578+enangkelvin@users.noreply.github.com>
2024-12-16 15:09:40 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const spicyChips = localFont({
 
 export const metadata: Metadata = {
   title: "Blue Bean",
-  description: "A decentralized social Experiment on Ton Ecosystem.",
+  description: "A decentralized social Experiment on Solana Ecosystem.",
 };
 
 export default function RootLayout({
